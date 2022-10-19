0a088dc6f004836ed0b94e7d92c57d661a09343b
HareK02 <kei.hiracchi.0928@gmail.com>
2022-10-19 21:44:11 +0900

    add main loop
    メインループを追加
    コメントアウトを追加

--- index.js
+++ index.js
@@ -62,13 +62,11 @@ let numFormats = {
     "short": [1000, ['k', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc', ' UnD', ' DoD', ' TrD', ' QaD', ' QiD', ' SxD', ' SpD', ' OcD', ' NoD', ' V', ' UnV', ' DoV', ' TrV', ' QaV', ' QiV', ' SxV', ' SpV', ' OcV', ' NoV', ' T', ' UnT', ' DoT', ' TrT', ' QaT', ' QiT', ' SxT', ' SpT', ' OcT', ' NoT', ' Qa', ' UnQa', ' DoQa', ' TrQa', ' QaQa', ' QiQa', ' SxQa', ' SpQa', ' OcQa', ' NoQa', ' Qi', ' UnQi', ' DoQi', ' TrQi', ' QaQi', ' QiQi', ' SxQi', ' SpQi', ' OcQi', ' NoQi', ' Sx', ' UnSx', ' DoSx', ' TrSx', ' QaSx', ' QiSx', ' SxSx', ' SpSx', ' OcSx', ' NoSx', ' Sp', ' UnSp', ' DoSp', ' TrSp', ' QaSp', ' QiSp', ' SxSp', ' SpSp', ' OcSp', ' NoSp', ' O', ' UnO', ' DoO', ' TrO', ' QaO', ' QiO', ' SxO', ' SpO', ' OcO', ' NoO', ' N', ' UnN', ' DoN', ' TrN', ' QaN', ' QiN', ' SxN', ' SpN', ' OcN', ' NoN']],
     "JA_JP": [10000, ['万', '億', '兆', '京', '垓', '𥝱', '穣', '溝', '澗', '正', '載', '極', '恒河沙', '阿僧祇', '那由他', '不可思議', '無量大数']]
 }
-//フォーマッタはどんな実装にしようか悩み中
-//日本語は４桁単位なのでフォーマッタを複数要るよな
 
 let Langs = {
     'EN_US': { nameEN: 'English', name: 'English' },
     'JA_JP': { nameEN: 'Japanese', name: '日本語' }
-} // lang/に配置されてる
+} // 予定はこの二言語だけ
 
 //#region game usualy function
 
@@ -86,8 +84,6 @@ let NumScale = (x) => {
     return Number.isFinite(x) ? ~~((toFixed(x).length + 1) / 3) : -1
 }
 
-//数値、揃える桁を与えると、成形された数値と単位を返す
-//1,234,000 2 = {"1.000","million"}
 let BeautifyNum = (value) => {
     let base = 0
     let scale = numFormats[Game.formatter][0]
@@ -102,27 +98,41 @@ let BeautifyNum = (value) => {
 //#endregion
 
 let Game = {}
-Game.SaveLoc = "SaveData"
-Game.SaveIndex = {
-    "basicInfo": 1,  //バージョン、言語など
-    "prefs": 2,      //設定項目
-    "game": 3,       //ゲームのデータ
-    "products": 4,   //施設設備の情報
-    "stats": 5       //実績
-}
 Game.Launch = () => {
-    Game.diamond = new BigInt('0')
 
-    //#region Click handling
-    Game.Click = () => {
-
-    }
-    //#endregion
 
     Game.Init = {} //初期化関数
+    //ゲームの機能自体はここに書いていく
     Game.Init = () => {
+        Game.diamond = new BigInt('0')
 
-        //#region Params&Prefs I/O
+        //#region Click handling
+        var ClickableDiamond = e('ClickableDiamond');
+        Game.Click = () => {
+
+        }
+        AddEvent(ClickableDiamond, 'click', Game.Click);
+        AddEvent(ClickableDiamond, 'mousedown', (e) => { });
+        AddEvent(ClickableDiamond, 'mouseup', (e) => { });
+        AddEvent(ClickableDiamond, 'mouseover', (e) => { });
+        AddEvent(ClickableDiamond, 'mouseout', (e) => { });
+        //#endregion
+
+        //#region GUI
+        var Title = e('diamonds');
+
+        //#endregion
+
+        //#region Game,prefs Data I/O
+        Game.SaveLoc = "SaveData"
+        Game.SaveIndex = {
+            "basicInfo": 1,  //バージョン、言語など
+            "prefs": 2,      //設定項目
+            "game": 3,       //ゲームのデータ
+            "products": 4,   //施設設備の情報
+            "stats": 5       //実績
+        }
+
         Game.prefs = []
         Game.prefsItems = [ //項目と並びの定義
             "particles",  // particle effects | 2:with physics / 1:normal particle / 0:off
@@ -132,7 +142,7 @@ Game.Launch = () => {
             "fancyvisual" // Visual with Code | 2:high / 1:mid / 0:low
         ]
         Game.defaultPrefs = [2, 75, 1, 2, 2] //項目のデフォルト値
-        Game.DefaultPrefs = () => {
+        Game.DefaultPrefs = () => { //その他初期値
             for (let i = 0; i < Game.prefsItems.length; i++) {
                 Game.prefs[Game.prefsItems[i]] = Game.defaultPrefs[i]
             }
@@ -205,12 +215,35 @@ Game.Launch = () => {
                 }
             })
         }
+
         //#endregion
+    
+        Game.gameSpeedManager = new GameSpeedManager(30)
+        Game.Loop();
+    }
+
+    //フレーム毎の処理
+    Game.Update = () => {
+        //dpsの加算、エフェクト効果の経過、
+    }
+
+    //描画
+    Game.Draw = () => {
+        //ダイヤ数の更新、エフェクトの処理
+    }
+
+    //メインループ
+    Game.Loop = () => {
+        Game.Update()
+        Game.Draw()
+
+        setTimeout(Game.Loop, gameSpeedManager.finish());
     }
 }
 window.onload = () => {
     Game.Launch()
     Game.Init()
     Game.LoadSave()
+
 };
 //#endregion
