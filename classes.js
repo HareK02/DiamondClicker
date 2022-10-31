88bb4f928f475b4b6e70f93e51c51fa248501835
HareK02 <kei.hiracchi.0928@gmail.com>
2022-10-31 16:43:19 +0900

    game loo test

--- classes.js
+++ classes.js
@@ -1,5 +1,6 @@
 class GameSpeedManager {
 
+<<<<<<< Updated upstream
     constructor(fps) {
         this.set_fps(fps);
     }
@@ -7,6 +8,20 @@ class GameSpeedManager {
     set_fps = function (fps) {
         this.goal_fps = fps;
         this.start_time = new Date().getTime();
+=======
+    constructor(tps, fps) {
+        this.set_param(tps, fps);
+    }
+
+    set_param = function (tps, fps) {
+        this.times_all_second = this.leastCommonMultiple(tps, fps);
+        this.times_tick_secound = this.times_all_second / tps;
+        this.times_frame_secound = this.times_all_second / fps;
+        this.interval = 1000 / this.times_all_second;
+        this.count_second = 0;
+
+        this.start_time = Date.now();
+>>>>>>> Stashed changes
         this.end_time = 0;
         this.total_first_time = this.start_time;
         this.total_count = 0;
@@ -14,8 +29,12 @@ class GameSpeedManager {
     }
 
     finish = function () {
+<<<<<<< Updated upstream
         this.end_time = new Date().getTime();
         this.total_count++;
+=======
+        this.end_time = Date.now();
+>>>>>>> Stashed changes
 
         if (this.total_count > this.goal_fps) {
             this.real_fps = 1000 * this.total_count / (this.end_time - this.total_first_time);
@@ -29,5 +48,54 @@ class GameSpeedManager {
         this.start_time = this.end_time + delta;
 
         return delta;
+    }
+<<<<<<< Updated upstream
+=======
+
+    shouldExecuteTick = function () {
+        let flag = this.count_second % this.times_tick_secound == 0;
+        if (flag) this.total_tick_count++;
+
+        return flag;
+    }
+    shouldExecuteDraw = function () {
+        let flag = this.count_second % this.times_frame_secound == 0;
+        if (flag) this.total_frame_count++;
+
+        return flag;
+    }
+
+    leastCommonMultiple = function (a, b) {
+        let i = Math.floor(Math.max(a, b));
+        let j = Math.floor(Math.min(a, b));
+
+        let l = i % j;
+        while (l != 0) {
+            i = j;
+            j = l;
+            l = i % j;
+        }
+
+        return a * b / j;
+    }
+>>>>>>> Stashed changes
+}
+
+class GameLoopManager {
+    constructor(func, cps) {
+        this.func = func
+        this.cps = cps
+        this.mpc = 1000 / this.cps
+    }
+    start() {
+        this.leastTime = Date.now()
+        done()
+    }
+
+    done() {
+        this.deltaTime = Date.now() - leastTime;
+        this.delta = mpc - this.deltaTime
+        this.leastTime = Date.now()
+        setTimeout(delta.func, 30);
     }
 }
